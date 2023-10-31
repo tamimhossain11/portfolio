d7814c5ffde53a82cc80396ec30bce955bb7096b
tamimhossain11 <amdtamim3@gmail.com>
2023-11-01 02:55:24 +0600

    updated

--- src/content_option.js
+++ src/content_option.js
@@ -20,105 +20,145 @@ const dataabout = {
     aboutme: "Enthusiastic and self-motivated Boy.Capable of handling responsibility & have good communication skills.Enjoing learning and applying new skills,Like to deal with challenges and apply my creative ideas. I dedicated myself to give the best service to my organization",
 };
 const worktimeline = [{
-        jobtitle: "Bangla",
-        where: "Mother Language",
-        
-    },
-    {
-        jobtitle: "English",
-        where: "Moderate",
-        
-    },
-    {
-        jobtitle: "Hindi",
-        where: "Can Speak",
-       
-    },
+    jobtitle: "Bangla",
+    where: "Mother Language",
+
+},
+{
+    jobtitle: "English",
+    where: "Moderate",
+
+},
+{
+    jobtitle: "Hindi",
+    where: "Can Speak",
+
+},
 ];
 
 const skills = [{
-        name: "HTML5",
-        value: 90,
-    },
-    {
-        name: "CSS",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 60,
-    },
-    {
-        name: "Java",
-        value: 70,
-    },
-    {
-        name: "J Query",
-        value: 40,
-    },
-    {
-        name: "Bootstrap",
-        value: 80,
-    },
-    {
-        name: "MY SQL",
-        value: 70,
-    },
-    {
-        name: "React",
-        value: 50,
-    },
-    {
-        name: "Angular",
-        value: 85,
-    },
+    name: "React.JS",
+    value: 70,
+},
+{
+    name: "Next.JS",
+    value: 55,
+},
+{
+    name: "Tailwind CSS",
+    value: 60,
+},
+{
+    name: "Angular",
+    value: 85,
+},
+
+{
+    name: "HTML5",
+    value: 80,
+},
+{
+    name: "CSS",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 70,
+},
+{
+    name: "Java",
+    value: 70,
+},
+{
+    name: "J Query",
+    value: 40,
+},
+{
+    name: "Bootstrap",
+    value: 80,
+},
+{
+    name: "MY SQL",
+    value: 70,
+},
+{
+    name: "React Native",
+    value: 30,
+},
 ];
 
 
 const dataportfolio = [{
-        projectName: "ABC Learning & Training Institute",
-        img: "https://i.ibb.co/L5SqWtk/M1.png",
-        desctiption: " (May- 2022) Designed and developed a website to provide information of the ABC Learning IT Training Institute (HTML, CSS, JavaScript, jQuery)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-    {
-        projectName: "MCQ System",
-        img: "https://i.ibb.co/4PdvLF1/M3.png" ,
-        desctiption: "(june - 2022) Developed an MCQ System for educational purposes.(IntelliJ)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-    {
-        projectName: "AAA Web Hosting with CMS",
-        img: "https://i.ibb.co/YbKn0ST/M8.png",
-        desctiption: "(September - 2022) Developed a website hosting service using (Liferay Liferay, MySQL)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-    {
-        projectName: "ABC Jobs Community Portal",
-        img: "https://i.ibb.co/kqVxMX3/M2.png",
-        desctiption: "(October - 2022) A website application that allows users to find a job and apply for job opportunities. Users can register and login to the website, search for another user, edit profiles, change password, and chat system. The application also includes administration that has the authority to manage the user data and also sends a bulk email with the email sender. (Spring MVC, JSP, CSS, Bootstrap, JavaScript, MySQL)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-    
-    {
-        projectName: "ABC CAR Portal",
-        img: "https://i.ibb.co/jGjy6Jn/M9.png",
-        desctiption: "(November - 2022) A website application that offers a marketplace for buying and selling used cars. It allows users to browse a wide selection of pre-owned vehicles and place bids on cars they are interested in, providing them with the potential to purchase a vehicle at a lower price.(Spring Boot, Spring Security, JSP, Bootstrap, JavaScript, MySQL, Spring Boot)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-    {
-        projectName: "Know Your Neighborhood",
-        img: "https://i.ibb.co/x5LZT3R/M10.png",
-        desctiption: "(December - 2022) A website application that provides users with information about their local neighborhood. The application allows users to register, login using a Facebook account through OAuth2, Post stores, search another store and edit their personal profile page. (ReactJS, Tailwind CSS, Spring Boot, Spring Security, JSON Web Token (JWT), Reacthook-form, Axios, MySQL)",
-        link: "#",
-    },
-    {
-        projectName: "Meals On Wheels",
-        img: "https://i.ibb.co/Dw9rQL1/M11.jpg",
-        desctiption: "(December - 2022) A website application that provides Food to the people who are unable to get food or prepare food. The application allows users to register, login,Donors to donate though API Call,partner to Post meals,And member to request meals . (ReactJS, Spring Boot, Spring Security, JSON Web Token (JWT), Reacthook-form, Axios, MySQL)",
-        link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
-    },
-   
+    projectName: "MyScope NFT",
+    img: "https://i.ibb.co/FK8ck50/Whats-App-Image-2023-10-14-at-23-57-27-d5ffe45b.jpg",
+    desctiption: " (October to November- 2023)An NFT marketplace project where my role is to develop the frontend with next.js also its a private client required project for enterprise perpouse.More upgrade and updation is in process...",
+    link: "https://github.com/tamimhossain11/",
+},
+{
+    projectName: "Crypto stake",
+    img: "https://i.ibb.co/r2KcYmN/Whats-App-Image-2023-11-01-at-02-28-33-ed22064a.jpg",
+    desctiption: " (september- 2023)A gamble project which frontend developed on React.js framework for providing users the top notch experience on gaming and using crypto online.More upgrade and updation is in process...",
+    link: "https://github.com/tamimhossain11/",
+},
+
+{
+    projectName: "AI Hub/Genius",
+    img: "https://i.ibb.co/P5ngSM0/screencapture-genius-saas-platform-vercel-app-2023-11-01-02-44-07.png",
+    desctiption: " (August- 2023)A Chatgpt ApI project, developed on Next.js framework for personal purpose and serving users who are surfing on google for several kinds of products like,AI image generation, AI video generation, AI photo generation and AI Audio generation and more facilities are under planning and development.",
+    link: "https://genius-saas-platform.vercel.app/",
+},
+
+{
+    projectName: "Know Your Neighborhood",
+    img: "https://i.ibb.co/x5LZT3R/M10.png",
+    desctiption: "(December - 2022) A website application that provides users with information about their local neighborhood. The application allows users to register, login using a Facebook account through OAuth2, Post stores, search another store and edit their personal profile page. (ReactJS, Tailwind CSS, Spring Boot, Spring Security, JSON Web Token (JWT), Reacthook-form, Axios, MySQL)",
+    link: "#",
+},
+{
+    projectName: "Meals On Wheels",
+    img: "https://i.ibb.co/Dw9rQL1/M11.jpg",
+    desctiption: "(December - 2022) A website application that provides Food to the people who are unable to get food or prepare food. The application allows users to register, login,Donors to donate though API Call,partner to Post meals,And member to request meals . (ReactJS, Spring Boot, Spring Security, JSON Web Token (JWT), Reacthook-form, Axios, MySQL)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+{
+    projectName: "ABC CAR Portal",
+    img: "https://i.ibb.co/jGjy6Jn/M9.png",
+    desctiption: "(November - 2022) A website application that offers a marketplace for buying and selling used cars. It allows users to browse a wide selection of pre-owned vehicles and place bids on cars they are interested in, providing them with the potential to purchase a vehicle at a lower price.(Spring Boot, Spring Security, JSP, Bootstrap, JavaScript, MySQL, Spring Boot)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+
+{
+    projectName: "ABC Learning & Training Institute",
+    img: "https://i.ibb.co/L5SqWtk/M1.png",
+    desctiption: " (May- 2022) Designed and developed a website to provide information of the ABC Learning IT Training Institute (HTML, CSS, JavaScript, jQuery)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+{
+    projectName: "MCQ System",
+    img: "https://i.ibb.co/4PdvLF1/M3.png",
+    desctiption: "(june - 2022) Developed an MCQ System for educational purposes.(IntelliJ)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+{
+    projectName: "AAA Web Hosting with CMS",
+    img: "https://i.ibb.co/YbKn0ST/M8.png",
+    desctiption: "(September - 2022) Developed a website hosting service using (Liferay Liferay, MySQL)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+{
+    projectName: "ABC Jobs Community Portal",
+    img: "https://i.ibb.co/kqVxMX3/M2.png",
+    desctiption: "(October - 2022) A website application that allows users to find a job and apply for job opportunities. Users can register and login to the website, search for another user, edit profiles, change password, and chat system. The application also includes administration that has the authority to manage the user data and also sends a bulk email with the email sender. (Spring MVC, JSP, CSS, Bootstrap, JavaScript, MySQL)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+
+{
+    projectName: "ABC CAR Portal",
+    img: "https://i.ibb.co/jGjy6Jn/M9.png",
+    desctiption: "(November - 2022) A website application that offers a marketplace for buying and selling used cars. It allows users to browse a wide selection of pre-owned vehicles and place bids on cars they are interested in, providing them with the potential to purchase a vehicle at a lower price.(Spring Boot, Spring Security, JSP, Bootstrap, JavaScript, MySQL, Spring Boot)",
+    link: "https://github.com/tamimhossain11/BDSE04-Lithan-projects",
+},
+
 ];
 
 const contactConfig = {
--- src/header/index.js
+++ src/header/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
@@ -7,11 +7,30 @@ import Themetoggle from "../components/themetoggle";
 
 const Headermain = () => {
   const [isActive, setActive] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
 
   const handleToggle = () => {
     setActive(!isActive);
     document.body.classList.toggle("ovhidden");
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsSmallScreen(true);
+        setActive(false);
+      } else {
+        setIsSmallScreen(false);
+        setActive(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <>
@@ -20,61 +39,70 @@ const Headermain = () => {
           <Link className="navbar-brand nav_ac" to="/">
             {logotext}
           </Link>
+          <Themetoggle />
           <div className="d-flex align-items-center">
-            <Themetoggle />
-            <button className="menu__button  nav_ac" onClick={handleToggle}>
-              {isActive ? <VscClose /> : <VscGrabber />}
-            </button>
+           
+            {isSmallScreen ? (
+              <button className="menu__button nav_ac" onClick={handleToggle}>
+                {!isActive ? <VscClose /> : <VscGrabber />}
+              </button>
+            ) : (
+              <div className="the_menu">
+                <Link to="/" className="my-3">
+                  Home
+                </Link>
+                <Link to="/portfolio" className="my-3">
+                  Project Experience
+                </Link>
+                <Link to="/about" className="my-3">
+                  About me
+                </Link>
+                <Link to="/contact" className="my-3">
+                  Contact me
+                </Link>
+              </div>
+            )}
           </div>
         </div>
 
-        <div className={`site__navigation ${isActive ? "menu__opend" : ""}`}>
-          <div className="bg__menu h-100">
-            <div className="menu__wrapper">
-              <div className="menu__container p-3">
-                <ul className="the_menu">
-                  <li className="menu_item">
-                    <Link onClick={handleToggle} to="/" className="my-3">
-                      Home
-                    </Link>
-                  </li>
-                  <li className="menu_item">
-                    <Link
-                      onClick={handleToggle}
-                      to="/portfolio"
-                      className="my-3"
-                    >
-                      Project Experience
-                    </Link>
-                  </li>
-                  <li className="menu_item">
-                    <Link onClick={handleToggle} to="/about" className="my-3">
-                      About me
-                    </Link>
-                  </li>
-                  <li className="menu_item">
-                    <Link onClick={handleToggle} to="/contact" className="my-3">
-                      Contact me
-                    </Link>
-                  </li>
-                </ul>
+        {isSmallScreen && (
+          <div className={`site__navigation ${!isActive ? "menu__opend" : ""}`}>
+            <div className="bg__menu h-100">
+              <div className="menu__wrapper">
+                <div className="menu__container p-3">
+                  <ul className="the_menu">
+                    <li className="menu_item">
+                      <Link onClick={handleToggle} to="/" className="my-3">
+                        Home
+                      </Link>
+                    </li>
+                    <li className="menu_item">
+                      <Link onClick={handleToggle} to="/portfolio" className="my-3">
+                        Project Experience
+                      </Link>
+                    </li>
+                    <li className="menu_item">
+                      <Link onClick={handleToggle} to="/about" className="my-3">
+                        About me
+                      </Link>
+                    </li>
+                    <li className="menu_item">
+                      <Link onClick={handleToggle} to="/contact" className="my-3">
+                        Contact me
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
-          <div className="menu_footer d-flex flex-column flex-md-row justify-content-between align-items-md-center position-absolute w-100 p-3">
-            <div className="d-flex">
-              <a href={socialprofils.facebook}>Facebook</a>
-              <a href={socialprofils.github}>Github</a>
-              <a href={socialprofils.twitter}>Twitter</a>
-            </div>
-            <p className="copyright m-0">copyright __ {logotext}</p>
-          </div>
-        </div>
+        )}
+
+        <div className="br-top"></div>
+        <div className="br-bottom"></div>
+        <div className="br-left"></div>
+        <div className="br-right"></div>
       </header>
-      <div className="br-top"></div>
-      <div className="br-bottom"></div>
-      <div className="br-left"></div>
-      <div className="br-right"></div>
     </>
   );
 };
